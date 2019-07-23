94064948cb7d3ae049aa08142b445beb2afce55b
Steve <chaunguyen4297@gmail.com>
2019-07-24 00:47:26 +0700

    fix

---
+++ EnglishApp/Entity/TheoryGroup/LessonCategoryEntity.swift
@@ -0,0 +1,24 @@
+//
+//  LessonEntity.swift
+//  EnglishApp
+//
+//  Created by Steve on 7/23/19.
+//  Copyright © 2019 demo. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class LessonCategoryEntity : Mappable {
+    var total_records : Int?
+    var categories : [ItemLessonCategory]?
+    
+    func mapping(map: Map) {
+        self.total_records <- map["total_records"]
+        self.categories <- map["categories"]
+    }
+    
+    required init?(map: Map) {
+        
+    }
+}
